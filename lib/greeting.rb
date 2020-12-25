8e0ccb54939c8043f6949902a2b8064df959b92a
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-25 17:12:51 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,5 @@
 # code the #greeting method here!
 def greeting(name)
-  puts "Hi! I\'m HAL, what\'s your name?"
-  name = gets.chomp()
   puts "Hello #{name}. It\'s nice to meet you."
+  puts "Hi! I\'m HAL, what\'s your name?"
 end
